da5280b95a0832f89822bab85e09ebe425718ebe
Abhisek8895 <mishraabhisek14@gmail.com>
2026-01-19 20:30:08 +0530

    Add multiple pdf loader function

---
+++ loaders/pdf_loader.py
@@ -0,0 +1,25 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.schema import Document
+import os
+
+
+def load_multiple_pdfs(pdf_dir: str):
+    """
+    Docstring for load_multiple_pdfs
+    
+    :param pdf_dir: Description
+    :type pdf_dir: str
+    """
+    documents = []
+
+    for file_name in os.listdir(pdf_dir):
+        if file_name.endswith(".pdf"):
+            file_path = os.path.join(pdf_dir, file_name)
+            loader = PyPDFLoader(file_path)
+            pdf_docs = loader.load()
+
+            for doc in pdf_docs:
+                doc.metadata["source"] = file_name
+                documents.append(doc)
+
+    return documents
