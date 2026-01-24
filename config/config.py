8e3914d5ae756664826ffc6ed2f1192832c2c341
Abhisek8895 <mishraabhisek14@gmail.com>
2026-01-24 21:46:20 +0530

    fix: update embedding model

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 GEMENI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-EMBEDDING_MODEL = "models/embedding-001"
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 LLM_MODEL = "gemini-1.5-flash"
 
 CHUNK_SIZE = 1000
