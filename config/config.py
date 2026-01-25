5bc9cde1a4aac6d3a711e4d35b13d2d0c534045d
Abhisek8895 <mishraabhisek14@gmail.com>
2026-01-25 15:43:32 +0530

    fix: update llm model name

--- config/config.py
+++ config/config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMENI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-LLM_MODEL = "gemini-1.5-flash"
+LLM_MODEL = "models/gemini-flash-latest"
 
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
